5669babcf14b653be9b254d33d8c8b266aa2c316
AnnaMargery <anna.szewc@gmail.com>
2023-04-27 20:15:20 +0200

    task2

--- String_tasks/src/main/java/org/example/App.java
+++ String_tasks/src/main/java/org/example/App.java
@@ -5,32 +5,38 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        String userOperation = getUserOperation();
-        Integer number = getVariables(userOperation);
-    }
-        public static String getUserOperation() {
-        System.out.println("Please enter mathematical operation number1 symbol (+, -, *, /) number2:");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+public class App {
+    public static void main(String[] args) {
+
+
+//        String userOperation = getUserOperation();
+//        Integer number = getVariables(userOperation);
+        messageWithoutSpaceAtTheEnd();
+        messageWithReplacedSpaces();
+
+
     }
 
-    public static Integer getVariables(String userOperation) {
-        String[] variables = userOperation.trim().split(" ");
-        Integer[] numbersSplit = new Integer[variables.length];
-        Integer number = 0;
-        for (Integer i=0; i< variables.length; i++) {
-            String s = variables.length[i];
-            Integer number = Integer.parseInt(s);
-            numbersSplit[i] = number;
-        }
-        System.out.println(number);
-        return number;
-        }
-    }
+//        public static String getUserOperation() {
+//        System.out.println("Please enter mathematical operation number1 symbol (+, -, *, /) number2:");
+//        Scanner scanner = new Scanner(System.in);
+//        return scanner.nextLine();
+//    }
+//
+//    public static String getVariables(String userOperation) {
+//        String[] variables = userOperation.trim().split(" ");
+//        Integer[] numbersSplit = new Integer[variables.length];
+//        Integer number = 0;
+//        for (Integer i=0; i< variables.length; i++) {
+//
+//            String s = variables[i];
+//            number = Integer.parseInt(s);
+//            numbersSplit[i] = number;
+//        }
+//        System.out.println(number);
+//        return number;
+//        }
+//    }
 
 
 //Kalkulator: napisz program kalkulator, który wspiera działania dodawania, odejmowania,
@@ -41,3 +47,28 @@ public class App
 // 4. W zależności od rodzaju operatora wykonujemy działanie
 // 5. Tworzymy metodę do wyśietlania wyniku
 
+//================================ zadanie 2=========================================
+
+    // 1. Usuniecie spacji z konca zdania
+    // 2. Zastapienie nadmiarowych spacji na pojedyncza
+
+    public static String messageWithoutSpaceAtTheEnd() {
+
+        String toClear = "Ala      ma              kota            ";
+        String withoutSpacesAtEnd = toClear.trim();
+        System.out.println(withoutSpacesAtEnd);
+        return withoutSpacesAtEnd;
+    }
+
+    public static String messageWithReplacedSpaces(String withoutSpacesAtEnd, String withoutSpaces) {
+
+
+        for(Integer i=0; i< )
+            withoutSpaces = withoutSpacesAtEnd.replace(" ", " ");
+
+        }
+        System.out.println(withoutSpaces);
+        return withoutSpaces;
+    }
+
+}
